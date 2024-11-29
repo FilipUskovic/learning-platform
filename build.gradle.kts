1bdf8ccf356c2cbba79cc31e59031681bc0b8083
Filip Uskovic <filip.uskovic@outlook.com>
2024-11-29 18:16:18 +0100

    kreirao dto course i entetije te mapper

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,6 +32,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.flywaydb:flyway-core")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
     /* kasnije cu korsiti
     implementation("org.springframework.boot:spring-boot-starter-webflux")
 
